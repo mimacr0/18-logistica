c79476bea6b40ddc71e319859ea90ad8bcaac812
AngelZH3 <angel.zh.3@gmail.com>
2026-03-19 14:46:02 +0100

    [UPDATE] Minor changes

--- rma_repair/models/product.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-from odoo import models, fields
-
-class ProductTemplate(models.Model):
-    _inherit = 'product.template'
-
-    is_spare_parts = fields.Boolean(string='Is a Spare Part', default=False)
